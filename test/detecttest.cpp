1092d4a375ffa67532fe3a14f96034911be977ae
Avvys <avvys@op.pl>
2014-11-14 16:47:39 +0100

    adding missed files

---
+++ test/detecttest.cpp
@@ -0,0 +1,42 @@
+/*
+ * detecttest.cpp
+ *
+ *  Created on: 7 lis 2014
+ *      Author: abby
+ */
+
+#include "detecttest.h"
+#include "../libs.h"
+#include "../info.h"
+
+detect_test::detect_test()
+{
+	_info("running test");
+	auto ok = detectWindow();
+	_dbg3(ok);
+}
+
+detect_test::~detect_test()
+{
+	// TODO Auto-generated destructor stub
+}
+
+bool detect_test::detectWindow() {
+	bool ok = false;
+	while (true) {
+		sleep(2);
+		auto win = info::GetCurrWindow();
+		if (win.first == "" || win.second == "") {
+			_erro("not detected");
+			ok = false;
+		}
+		else {
+			_dbg2(win.first << " - " << win.second);
+			ok = true;
+		}
+	}
+	return ok;
+}
+
+bool detect_test::detectWorkspace() {
+}
---
+++ test/detecttest.h
@@ -0,0 +1,20 @@
+/*
+ * detecttest.h
+ *
+ *  Created on: 7 lis 2014
+ *      Author: abby
+ */
+
+#ifndef DETECTTEST_H_
+#define DETECTTEST_H_
+
+class detect_test {
+public:
+	detect_test();
+	virtual ~detect_test();
+
+	bool detectWindow();
+	bool detectWorkspace();
+};
+
+#endif /* DETECTTEST_H_ */
